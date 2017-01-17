524c5ffb6f90aa6a313249fe38484ccc5b0b0622
Ramyar <ramyar6@hotmail.com>
2017-01-17 18:22:23 +0100

    d

--- app/src/main/java/com/example/ramyar/hangman/MulitPlayerChooseWord_frag.java
+++ app/src/main/java/com/example/ramyar/hangman/MulitPlayerChooseWord_frag.java
@@ -11,48 +11,40 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
-/**
- * Created by Ramyar on 14-01-2017.
- */
 
-public class MulitPlayerChooseWord_frag extends Fragment {
-
+public class MulitPlayerChooseWord_frag extends Fragment
+{
     private ListView listView;
     public TextView multiplayer_text;
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+                             Bundle savedInstanceState)
+    {
         View view = inflater.inflate(R.layout.frag_multi_player_choose_word, container, false);
-
         return view;
     }
 
 
-    public void onViewCreated(View view , Bundle savedInstantceState){
-
+    public void onViewCreated(View view , Bundle savedInstantceState)
+    {
         multiplayer_text = (TextView) view.findViewById(R.id.multiplayer_text);
-
         listView = (ListView) view.findViewById(R.id.listView1);
 
         final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this.getContext(), android.R.layout.simple_list_item_1, Constants.wordList);
 
         listView.setAdapter(arrayAdapter);
 
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener()
+        {
             @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id)
+            {
                 Object object = listView.getItemAtPosition(position);
                 String selectedWord = object.toString();
                 Constants.ordet = selectedWord;
-
                 Intent intent = new Intent(getActivity(), SingelPlayerActivity.class);
-
                 startActivity(intent);
-
-
-
             }
         });
     }
--- app/src/main/java/com/example/ramyar/hangman/SingelPlayerActivity.java
+++ app/src/main/java/com/example/ramyar/hangman/SingelPlayerActivity.java
@@ -1,32 +1,26 @@
 package com.example.ramyar.hangman;
 
-import android.app.AlertDialog;
-import android.content.Context;
-import android.content.DialogInterface;
-import android.content.SharedPreferences;
-import android.net.Uri;
-import android.os.SystemClock;
-import android.support.v4.app.DialogFragment;
-import android.support.v4.app.FragmentTransaction;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
-import android.widget.Chronometer;
-import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.TextView;
+        import android.app.AlertDialog;
+        import android.content.DialogInterface;
+        import android.net.Uri;
+        import android.os.CountDownTimer;
+        import android.support.v7.app.AppCompatActivity;
+        import android.os.Bundle;
+        import android.view.Menu;
+        import android.view.MenuItem;
+        import android.view.View;
+        import android.widget.Button;
+        import android.widget.EditText;
+        import android.widget.ImageView;
+        import android.widget.TextView;
+        import com.google.android.gms.appindexing.Action;
+        import com.google.android.gms.appindexing.AppIndex;
+        import com.google.android.gms.appindexing.Thing;
+        import com.google.android.gms.common.api.GoogleApiClient;
 
-import com.google.android.gms.appindexing.Action;
-import com.google.android.gms.appindexing.AppIndex;
-import com.google.android.gms.appindexing.Thing;
-import com.google.android.gms.common.api.GoogleApiClient;
 
-import java.util.concurrent.TimeUnit;
-
-public class SingelPlayerActivity extends AppCompatActivity {
+public class SingelPlayerActivity extends AppCompatActivity implements View.OnClickListener
+{
 
     private int life = 0;
     private Button gætBtn;
@@ -34,28 +28,30 @@ public class SingelPlayerActivity extends AppCompatActivity {
     private EditText editText;
     private ImageView imageView;
     public TextView multiplayer_text;
-    Chronometer chronometer;
-
-
-    Logic logik;
-    /**
-     * ATTENTION: This was auto-generated to implement the App Indexing API.
-     * See https://g.co/AppIndexing/AndroidStudio for more information.
-     */
     private GoogleApiClient client;
+    CountDownTimer countDownTimer;
+    int points = 3000;
+    TextView timer;
+    TextView timerpoints;
+    Logic logik;
+
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_start);
+        setContentView(R.layout.activity_singel_player);
 
+        timer = (TextView) findViewById(R.id.Timer);
+        timerpoints = (TextView) findViewById(R.id.timerpoints);
+
+        gætBtn = (Button) findViewById(R.id.gætBtn);
+        gætBtn.setOnClickListener(this);
 
         logik = new Logic();
         logik.logStatus();
+        pointTimer(180, timer);
 
-        chronometer = (Chronometer) findViewById(R.id.chronometer);
-        chronometer.setBase(SystemClock.elapsedRealtime());
-        chronometer.start();
         multiplayer_text = (TextView) findViewById(R.id.multiplayer_text);
         imageView = (ImageView) findViewById(R.id.imageView);
         gætBtn = (Button) findViewById(R.id.gætBtn);
@@ -65,45 +61,49 @@ public class SingelPlayerActivity extends AppCompatActivity {
                 "\nSkriv et bogstav og tryk 'Gæt'.\n");
 
 
-        if (Constants.gameType.equals("SingelPlayer")) {
+        if (Constants.gameType.equals("SingelPlayer"))
+        {
             multiplayer_text.setVisibility(View.INVISIBLE);
-        } else {
+        }
+        else
+        {
             multiplayer_text.setVisibility(View.VISIBLE);
         }
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
+
         client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
 
-    public boolean onCreateOptionsMenu(Menu menu) {
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
         getMenuInflater().inflate(R.menu.menu, menu);
         return super.onCreateOptionsMenu(menu);
-
-
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        if (item.getItemId() == R.id.menu_genstart) {
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        if (item.getItemId() == R.id.menu_genstart)
+        {
             AlertDialog.Builder builder = new AlertDialog.Builder(this);
             builder.setCancelable(false);
             builder.setMessage("Vil du genstarte spillet?");
 
-            builder.setPositiveButton("Ja", new DialogInterface.OnClickListener() {
+            builder.setPositiveButton("Ja", new DialogInterface.OnClickListener()
+            {
                 @Override
-                public void onClick(DialogInterface dialog, int which) {
-                    //if user pressed "JA", then he is allowed to exit from application
+                public void onClick(DialogInterface dialog, int which)
+                {
                     finish();
-
-//                    restart();
+                    restart();
                 }
             });
 
-            builder.setNegativeButton("Nej", new DialogInterface.OnClickListener() {
+            builder.setNegativeButton("Nej", new DialogInterface.OnClickListener()
+            {
                 @Override
-                public void onClick(DialogInterface dialog, int which) {
-                    //if user select "nej", just cancel this dialog and continue with app
+                public void onClick(DialogInterface dialog, int which)
+                {
                     dialog.cancel();
                 }
             });
@@ -115,8 +115,8 @@ public class SingelPlayerActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    private void restart() {
-
+    private void restart()
+    {
         logik.nulstil();
         imageView.setImageResource(R.drawable.galge);
         editText.setText("");
@@ -124,22 +124,24 @@ public class SingelPlayerActivity extends AppCompatActivity {
         editText.setHint("Indsæt Bogstav");
         textView.setText("Du skal gætte dette ord: " + logik.getSynligtOrd() +
                 "\nSkriv et bogstav og tryk 'Gæt'.\n");
-
     }
 
 
-    private void updateScreen() {
-
+    private void updateScreen()
+    {
         textView.setText("Gæt ordet: " + logik.getSynligtOrd());
         textView.append("\n\nDu har " + logik.getAntalForkerteBogstaver() + " forkerte:" + logik.getBrugteBogstaver());
 
-        if (!logik.erSidsteBogstavKorrekt()) {
+        if (!logik.erSidsteBogstavKorrekt())
+        {
             life++;
-            if (life >= 6) {
+            if (life >= 6)
+            {
                 life = 6;
             }
 
-            switch (life) {
+            switch (life)
+            {
                 case 1:
                     imageView.setImageResource(R.drawable.forkert1);
                     break;
@@ -163,67 +165,35 @@ public class SingelPlayerActivity extends AppCompatActivity {
             }
         }
 
-        if (logik.erSpilletVundet()) {
-            chronometer.stop();
-
+        if (logik.erSpilletVundet())
+        {
             textView.append("\nDu har vundet");
             editText.setHint("Tryk Genstart for at spile igen");
-
+            countDownTimer.cancel();
             saveHighScore();
-
-
         }
-        if (logik.erSpilletTabt()) {
-            chronometer.stop();
+        if (logik.erSpilletTabt())
+        {
             textView.setText("Du har tabt, det rigtige ord er : " + logik.getOrdet());
             editText.setHint("Tryk Genstart for at spile igen");
-            saveHighScore();
+            countDownTimer.cancel();
         }
     }
 
-    public void onClickKill(View v) {
-        String bogstav = editText.getText().toString();
 
-        if (bogstav.length() != 1) {
-            editText.setError("Skriv ét bogstav");
-            return;
-        }
-        logik.gætBogstav(bogstav);
-        editText.setText("");
-        editText.setError(null);
-        updateScreen();
-
-    }
-
-    public void saveHighScore() {
-        SharedPreferences sharedPreferences = getSharedPreferences("highScore", Context.MODE_PRIVATE);
-        SharedPreferences.Editor editor = sharedPreferences.edit();
-        editor.putString("name", Constants.name);
-        editor.putString("time", getElapsedTime());
-        editor.commit();
-    }
-
-    public String getElapsedTime() {
-
-        long elapsedMillis = SystemClock.elapsedRealtime() - chronometer.getBase();
-        String time = Long.toString(elapsedMillis);
-        time = time.format("%02d:%02d",
-                TimeUnit.MILLISECONDS.toMinutes(elapsedMillis),
-                TimeUnit.MILLISECONDS.toSeconds(elapsedMillis) - TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(elapsedMillis))
-        );
-
-        return time;
+    public void saveHighScore()
+    {
+        HighScoreLogic highScoreLogic = new HighScoreLogic(this);
+        highScoreLogic.setHighScore(Constants.name, points);
+        System.out.println(Constants.name +" " + points );
     }
 
 
-    /**
-     * ATTENTION: This was auto-generated to implement the App Indexing API.
-     * See https://g.co/AppIndexing/AndroidStudio for more information.
-     */
-    public Action getIndexApiAction() {
+
+    public Action getIndexApiAction()
+    {
         Thing object = new Thing.Builder()
-                .setName("SingelPlayer Page") // TODO: Define a title for the content shown.
-                // TODO: Make sure this auto-generated URL is correct.
+                .setName("SingelPlayer Page")
                 .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
                 .build();
         return new Action.Builder(Action.TYPE_VIEW)
@@ -233,25 +203,65 @@ public class SingelPlayerActivity extends AppCompatActivity {
     }
 
     @Override
-    public void onStart() {
+    public void onStart()
+    {
         super.onStart();
-
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
         client.connect();
         AppIndex.AppIndexApi.start(client, getIndexApiAction());
     }
 
     @Override
-    public void onStop() {
+    public void onStop()
+    {
         super.onStop();
-
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
         AppIndex.AppIndexApi.end(client, getIndexApiAction());
         client.disconnect();
     }
 
 
+    public void pointTimer(int Seconds,final TextView tv)
+    {
+        countDownTimer = new CountDownTimer(Seconds* 1000+1000, 500)
+        {
+            public void onTick(long millisUntilFinished)
+            {
+                int seconds = (int) (millisUntilFinished / 1000);
+                int minutes = seconds / 60;
+                seconds = seconds % 60;
+                tv.setText(String.format("%02d", minutes) + ":" + String.format("%02d", seconds));
+                points -= 10;
+                timerpoints.setText("Point tilbage: " + points);
+            }
+
+            public void onFinish()
+            {
+                logik.isGameLost(true);
+                textView.setText("Du har tabt, det rigtige ord er : " + logik.getOrdet());
+                editText.setHint("Tryk Genstart for at spile igen");
+            }
+
+        }.start();
+    }
+
+
+    @Override
+    public void onClick(View v)
+    {
+        if (v == gætBtn)
+        {
+            String bogstav = editText.getText().toString();
+
+            if (bogstav.length() > 1)
+            {
+                editText.setError("Skriv ét bogstav");
+                return;
+            }
+
+            logik.gætBogstav(bogstav);
+            editText.setText("");
+            editText.setError(null);
+            updateScreen();
+        }
+    }
 }
 
